5601305045e486d3da99c5f73010f2ccc3928abd
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-01-13 11:09:31 +0700

    add config

---
+++ src/utils/config.js
@@ -0,0 +1,16 @@
+const config = {
+  app: {
+
+  },
+  s3: {
+
+  }, 
+  rabbitMq: {
+    server: process.env.RABBITMQ_SERVER,
+  },
+  redis: {
+    host: process.env.REDIS_SERVER,
+  },
+};
+
+module.exports = config;
